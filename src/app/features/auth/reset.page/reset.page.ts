837ccfe712038e6a4433913b1f8476a9cf0ce9ee
el2toro <vasea.badragan@gmail.com>
2025-11-27 09:50:31 +0100

    add reset password page

---
+++ src/app/features/auth/reset.page/reset.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reset.page',
+  imports: [],
+  templateUrl: './reset.page.html',
+  styleUrl: './reset.page.scss',
+})
+export class ResetPage {
+
+}
