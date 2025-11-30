9d0c673452fcea5d91fdb7ac19c97e283954e19b
el2toro <vasea.badragan@gmail.com>
2025-11-30 12:50:51 +0100

    update header

--- src/app/features/layout/header/header.component.ts
+++ src/app/features/layout/header/header.component.ts
@@ -1,26 +1,33 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { MiniCart } from "../../../shared/components/mini-cart/mini-cart";
+import { MiniCart } from '../../../shared/components/mini-cart/mini-cart';
 import { AuthStore } from '../../../core/auth/auth.store';
-import { Observable } from 'rxjs';
+import { RouterModule } from '@angular/router';
+import { SideMenuComponent } from '../side-menu/side-menu.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  imports: [MatIconModule, CommonModule, MiniCart]
+  imports: [MatIconModule, CommonModule, MiniCart, RouterModule, SideMenuComponent],
 })
 export class HeaderComponent implements OnInit {
-  auth = inject(AuthStore)
+  auth = inject(AuthStore);
+  menuOpen = false;
 
-  get isAuthenticated() : boolean{
+  get isAuthenticated(): boolean {
     return this.auth.isAuthenticated();
   }
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-    
+  ngOnInit() {}
+
+  openMenu() {
+    this.menuOpen = true;
   }
 
+  closeMenu() {
+    this.menuOpen = false;
+  }
 }
