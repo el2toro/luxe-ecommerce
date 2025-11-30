1267b227856640e63cd8fa6265416656619657dc
el2toro <vasea.badragan@gmail.com>
2025-11-30 12:52:28 +0100

    update profile component

--- src/app/features/account/account.page/account.page.ts
+++ src/app/features/account/account.page/account.page.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { BottomNavComponent } from "../../layout/bottom-nav/bottom-nav.component";
 import { HeaderComponent } from "../../layout/header/header.component";
 import { MatIconModule } from '@angular/material/icon';
+import { AuthStore } from '../../../core/auth/auth.store';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-account.page',
@@ -10,5 +12,10 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './account.page.scss',
 })
 export class AccountPage {
-
+private authStore = inject(AuthStore);
+private router = inject(Router);
+  signOut(){
+   this.authStore.logout();
+   this.router.navigate(['/']);
+  }
 }
