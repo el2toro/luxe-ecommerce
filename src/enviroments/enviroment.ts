a2171ad017aca5fb9d91cfdc0d3480dc7017b82f
el2toro <vasea.badragan@gmail.com>
2025-12-05 17:30:00 +0100

    configure  enviromets

---
+++ src/enviroments/enviroment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+   production: true,
+   stripe: {
+    publishableKey: 'pk_test_51Qh6JVKybwKhiDNn46U0u5dCjeuSqUBAJyqHdonltlyT9XM9eC8zpKkQclhs0Tf3zSpeTzxn2CSA0jASQIKRGMlV001oQ4jnEP'
+   }
+}
