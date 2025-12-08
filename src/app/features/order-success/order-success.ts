8c56a7b6a2d3e08b7133ffb6b6f4d2ab9438b2da
el2toro <vasea.badragan@gmail.com>
2025-12-08 22:36:16 +0100

    enhance order succes component

--- src/app/features/order-success/order-success.ts
+++ src/app/features/order-success/order-success.ts
@@ -1,19 +1,34 @@
-import { Component, inject } from '@angular/core';
-import { BottomNavComponent } from "../layout/bottom-nav/bottom-nav.component";
+import { Component, inject, OnInit } from '@angular/core';
 import { HeaderComponent } from "../layout/header/header.component";
 import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { AsyncPipe, CommonModule, CurrencyPipe, DatePipe } from '@angular/common';
-import { OrderService } from '../../core/services/order.service';
+import { OrderingService } from '../../core/services/ordering.service';
+import { CheckoutStore } from '@core/store/checkout.store';
 
 @Component({
   selector: 'app-order-success',
-  imports: [AsyncPipe, DatePipe, CurrencyPipe, RouterModule, HeaderComponent, BottomNavComponent, MatIconModule, CommonModule],
+  imports: [AsyncPipe, DatePipe, CurrencyPipe, RouterModule, HeaderComponent, MatIconModule, CommonModule],
   templateUrl: './order-success.html',
   styleUrl: './order-success.scss',
 })
-export class OrderSuccess {
-  private orderService = inject(OrderService);
-order$ = this.orderService.currentOrder$;
+export class OrderSuccess implements OnInit {
+  private checkoutStore = inject(CheckoutStore);
+  private orderingService = inject(OrderingService);
+  order$ = this.orderingService.currentOrderDetails$;
+
   constructor() {}
+
+  ngOnInit(): void {
+    this.getCreatedOrder();
+  }
+
+  getCreatedOrder(){
+    this.checkoutStore.vm$.subscribe({
+      next: (checkoutState) => {
+        console.log('orderId: ', checkoutState.orderId)
+        this.orderingService.getOrderById(checkoutState.orderId)
+      }
+    })
+  }
 }
