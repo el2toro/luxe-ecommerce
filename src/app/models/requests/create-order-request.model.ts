aa6713cdc9f773fdfb9a45169edf76728e79de0c
el2toro <vasea.badragan@gmail.com>
2025-12-09 21:04:48 +0100

    use interface instead of class

--- src/app/models/requests/create-order-request.model.ts
+++ src/app/models/requests/create-order-request.model.ts
@@ -1,11 +1,11 @@
 import { AddressModel } from '@models/address.model';
 import { OrderItem } from '@models/order-item.model';
 
-export class CreateOrderRequestModel {
-  customerId!: string;
-  shippingAddress!: AddressModel;
-  billingAddress!: AddressModel;
-  currency!: string;
-  customerNotes!: string;
-  orderItems = <OrderItem[]>[];
+export interface CreateOrderRequestModel {
+  customerId: string;
+  shippingAddress: AddressModel;
+  billingAddress: AddressModel;
+  currency: string;
+  customerNotes: string;
+  orderItems: OrderItem[];
 }
