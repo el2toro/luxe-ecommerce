102c2425aebb621f1dc0f65d1652c5d0e4926cda
el2toro <vasea.badragan@gmail.com>
2025-12-13 18:36:06 +0100

    integrate backend in login flow

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,46 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+  FormsModule,
+} from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 import { AuthStore } from '../../../core/auth/auth.store';
+import { AuthModel as AuthRequestModel } from '@models/auth/auth-request.model';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  imports: [ReactiveFormsModule, MatIconModule, CommonModule, FormsModule, RouterModule ]
+  imports: [ReactiveFormsModule, MatIconModule, CommonModule, FormsModule, RouterModule],
 })
 export class LoginComponent implements OnInit {
   showPass = false;
-  remember = true;
-  form!:  FormGroup;
+  form!: FormGroup;
 
-  constructor(
-    private fb: FormBuilder,
-    private auth: AuthStore,
-    private router: Router
-  ) {}
+  constructor(private fb: FormBuilder, private auth: AuthStore, private router: Router) {}
   ngOnInit(): void {
-    this.form = this.fb.group({
-    email: [null, Validators.required],
-    password: [null, Validators.required]
-  });
+    this.buildForm();
   }
 
   login() {
-    // Fake success (replace with real API call)
-    this.auth.login('fake-jwt-token-123', {
-      id: '1',
-      name: 'Alexander Voss',
-      email: this.form.controls['email'].value,
-      isElite: true,
-      password: this.form.controls['password'].value
-    }).subscribe({
+    const authRequest: AuthRequestModel = { ...this.form.value };
+    this.auth.login(authRequest).subscribe({
       next: () => {
         this.router.navigate(['/']);
       },
-      error: (error) => console.log(error)
+      error: (error) => console.log(error),
+    });
+  }
+
+  buildForm() {
+    this.form = this.fb.group({
+      email: [null, Validators.required],
+      password: [null, Validators.required],
+      rememberMe: [false],
     });
   }
 }
