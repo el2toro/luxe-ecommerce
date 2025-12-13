2825342b6e0e4e0b871de7fe757e8de4adb82eb9
el2toro <vasea.badragan@gmail.com>
2025-12-13 18:33:03 +0100

    update mini cart

--- src/app/shared/components/mini-cart/mini-cart.ts
+++ src/app/shared/components/mini-cart/mini-cart.ts
@@ -1,20 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatIconModule } from "@angular/material/icon";
-import { AsyncPipe } from '@angular/common';
-import { CartStore } from '../../../core/store/cart.store';
+import { CartService } from '@core/services/cart.service';
 
 @Component({
   selector: 'app-mini-cart',
-  imports: [MatIconModule, CommonModule, AsyncPipe],
+  imports: [MatIconModule, CommonModule, ],
   templateUrl: './mini-cart.html',
   styleUrl: './mini-cart.scss',
 })
-export class MiniCart {
-  private cart = inject(CartStore);
-count$ = this.cart.totalItems$;
+export class MiniCart implements OnInit {
+  private cart = inject(CartService);
+  count$ = this.cart.currentCartItems$;
 
-  constructor() {}
+  ngOnInit(): void {
+    this.cart.getCart('c9f1f7bd-7a2e-4581-96b0-e017069c895e');
+  }
+
   openCart() {
     document.body.classList.add('cart-open');
   }
