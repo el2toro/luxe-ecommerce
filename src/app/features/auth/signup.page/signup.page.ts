37a2993f693fe46f9adb8159872dac749a6e947f
el2toro <vasea.badragan@gmail.com>
2025-12-15 14:54:49 +0100

    implement signup functionality

--- src/app/features/auth/signup.page/signup.page.ts
+++ src/app/features/auth/signup.page/signup.page.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthStore } from '../../../core/auth/auth.store';
 import { Router, RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
@@ -13,24 +13,15 @@ import { CommonModule } from '@angular/common';
 })
 export class SignupPage implements OnInit {
   private fb = inject(FormBuilder);
+  private auth = inject(AuthStore);
+  private router = inject(Router);
   showPass = false;
   strength = 0;
   strengthText = 'Password strength';
+  form!: FormGroup;
 
-  form = this.fb.group({
-    name: ['', Validators.required],
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(8)]],
-    terms: [false, Validators.requiredTrue],
-  });
-
-  constructor(private auth: AuthStore, private router: Router) {
-    this.form.get('password')?.valueChanges.subscribe((pass) => {
-      this.calculateStrength(pass || '');
-    });
-  }
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.buildForm();
   }
 
   calculateStrength(pass: string) {
@@ -46,8 +37,23 @@ export class SignupPage implements OnInit {
   }
 
   signup() {
-    // Mock success
-   // this.auth.signup();
-    this.router.navigate(['/']);
+    const signupData = {...this.form.value};
+    this.auth.signup(signupData).subscribe({
+      next: (res) => this.router.navigate(['/login']),
+      error: (err) => console.error('Signup failed', err),
+    });
+  }
+
+  buildForm(){
+  this.form = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    terms: [false, Validators.requiredTrue],
+  });
+
+  this.form.get('password')?.valueChanges.subscribe((pass) => {
+      this.calculateStrength(pass || '');
+    });
   }
 }
