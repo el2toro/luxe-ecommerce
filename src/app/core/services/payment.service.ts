d16c4d20e312433d26c7b9a44e818f1deb9706ef
el2toro <vasea.badragan@gmail.com>
2026-02-19 10:49:15 +0100

    update payment service url

--- src/app/core/services/payment.service.ts
+++ src/app/core/services/payment.service.ts
@@ -6,7 +6,7 @@ import { PaymentIntentModel } from "../../models/payment-intent.model";
 @Injectable({ providedIn: 'root' })
 export class PaymentService {
     private httpClient = inject(HttpClient);
-    private baseUrl = 'https://localhost:7163/api';
+    private baseUrl = 'http://localhost:5144/api';
 
   createPaymentIntent(paymentIntent: PaymentIntentModel): Observable<any>{
     return this.httpClient.post<any>(`${this.baseUrl}/create-payment-intent`, paymentIntent);
