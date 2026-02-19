4f8709b0d52d0a2735c65c8907eb46af18fbc43a
el2toro <vasea.badragan@gmail.com>
2026-02-19 19:27:13 +0100

    fix cart service methods

--- src/app/core/services/cart.service.ts
+++ src/app/core/services/cart.service.ts
@@ -1,39 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { CartModel } from '@models/cart.model';
-import { BehaviorSubject, map, Observable } from 'rxjs';
+import { BehaviorSubject, map } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class CartService {
   private baseUrl = 'http://localhost:7000/cart-service/cart';
   private httpClient = inject(HttpClient);
-private cart = new BehaviorSubject<CartModel>({id: '', customerId: '', cartItems: [], total: 0, subtotal: 0});
-private cartItems = new BehaviorSubject<number>(0);
-currentCart$ = this.cart.asObservable();
-currentCartItems$ = this.cartItems.asObservable();
+ private cart = new BehaviorSubject<CartModel>({id: '', customerId: '', cartItems: [], total: 0, subtotal: 0});
+ currentCart$ = this.cart.asObservable();
 
   addItemToCart(cartRequest: any) {
     this.httpClient.post<CartModel>(`${this.baseUrl}/item`, cartRequest, { withCredentials: true })
-   .pipe(map((cart) => {
-        this.cart.next(cart);
-        this.cartItems.next(cart.cartItems.length);
-   })).subscribe();
+   .pipe(map((cart) =>  this.cart.next(cart)))
+   .subscribe();
   }
 
   removeFromCart(cartRequest: any) {
     this.httpClient
      .put<CartModel>(`${this.baseUrl}/${'cartId'}/items/${cartRequest.cartId}`, cartRequest, { withCredentials: true })
-     .pipe(map((cart) => {
-        this.cart.next(cart);
-        this.cartItems.next(cart.cartItems.length);
-   })).subscribe();
+     .pipe(map((cart) => this.cart.next(cart)))
+     .subscribe();
   }
 
-  getCart(customerId: string){
-    this.httpClient.get<CartModel>(`${this.baseUrl}/${customerId}`, { withCredentials: true })
-    .pipe(map((cart) => {
-        this.cart.next(cart)
-        this.cartItems.next(cart.cartItems.length)
-    })).subscribe()
+  getCart(){
+    this.httpClient.get<CartModel>(`${this.baseUrl}`, { withCredentials: true })
+    .pipe(map((cart) => this.cart.next(cart)))
+    .subscribe()
   }
 }
