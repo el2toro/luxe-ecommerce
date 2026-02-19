53a23d035ffa92f81b71831e66d9def0d52f176e
el2toro <vasea.badragan@gmail.com>
2026-02-19 10:48:25 +0100

    implement remove from cart service method

--- src/app/core/services/cart.service.ts
+++ src/app/core/services/cart.service.ts
@@ -3,19 +3,27 @@ import { inject, Injectable } from '@angular/core';
 import { CartModel } from '@models/cart.model';
 import { BehaviorSubject, map, Observable } from 'rxjs';
 
-
 @Injectable({ providedIn: 'root' })
 export class CartService {
-  private baseUrl = 'https://localhost:7070/cart-service/cart';
+  private baseUrl = 'http://localhost:7000/cart-service/cart';
   private httpClient = inject(HttpClient);
 private cart = new BehaviorSubject<CartModel>({id: '', customerId: '', cartItems: [], total: 0, subtotal: 0});
 private cartItems = new BehaviorSubject<number>(0);
 currentCart$ = this.cart.asObservable();
 currentCartItems$ = this.cartItems.asObservable();
 
-  addItemToCart(item: any) {
-    this.httpClient.post<CartModel>(`${this.baseUrl}/item`, item, { withCredentials: true })
+  addItemToCart(cartRequest: any) {
+    this.httpClient.post<CartModel>(`${this.baseUrl}/item`, cartRequest, { withCredentials: true })
    .pipe(map((cart) => {
+        this.cart.next(cart);
+        this.cartItems.next(cart.cartItems.length);
+   })).subscribe();
+  }
+
+  removeFromCart(cartRequest: any) {
+    this.httpClient
+     .put<CartModel>(`${this.baseUrl}/${'cartId'}/items/${cartRequest.cartId}`, cartRequest, { withCredentials: true })
+     .pipe(map((cart) => {
         this.cart.next(cart);
         this.cartItems.next(cart.cartItems.length);
    })).subscribe();
