37707ce581a02fd661a7334fe1b4dece9f57413e
el2toro <vasea.badragan@gmail.com>
2026-03-05 08:31:43 +0100

    update payment endpoint

--- src/app/core/services/payment.service.ts
+++ src/app/core/services/payment.service.ts
@@ -6,7 +6,7 @@ import { PaymentIntentModel } from "../../models/payment-intent.model";
 @Injectable({ providedIn: 'root' })
 export class PaymentService {
     private httpClient = inject(HttpClient);
-    private baseUrl = 'http://localhost:5144/api';
+    private baseUrl = 'http://localhost:7000/payment-service/api';
 
   createPaymentIntent(paymentIntent: PaymentIntentModel): Observable<any>{
     return this.httpClient.post<any>(`${this.baseUrl}/create-payment-intent`, paymentIntent);
